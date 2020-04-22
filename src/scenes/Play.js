b5b409bf522de32c605cbd42020242de4ebf6fb0
Mark <markmdvd@gmail.com>
2020-04-22 13:09:23 -0700

    temp assets.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,12 +5,21 @@ class Play extends Phaser.Scene {
 
     preload() {
         //load up all assets and animations
+        this.load.image('skyBG','../../assets/art/SkyBackground.jpg');
+        this.load.image('treeBG','../../assets/art/treeBackgrounLayer.png');
+        this.load.image('mountainBG','../../assets/art/mountainBackgrounLayer.png');
+        this.load.image('player','../../assets/art/player.png');
+
     }
 
     create() {
         game.input.mouse.capture = true;
 
         //place backgrounds
+        this.skyBG = this.add.tileSprite(0,0,640,480,'skyBG').setOrigin(0,0);
+        this.mountainBG = this.add.tileSprite(0,0,game.config.width,game.config.height/2,'mountainBG').setOrigin(0,0).setScale(1.25,1.25);
+        this.treeBG = this.add.tileSprite(0,0,game.config.width,game.config.height/2,'treeBG').setOrigin(0,0).setScale(1.25,1.25);
+        this.p1 = new Player(this,20,game.config.height-100,'player').setOrigin(.5,.5);
 
         //place assets into the scene
     }
